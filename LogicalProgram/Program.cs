96787915781ac0ed8f6ef777d7031834e5bf0541
Anandmohanty <mohantyanand3@gmail.com>
2023-02-18 21:15:57 +0530

    [Anand] - Perfect Number

--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -2,30 +2,38 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        internal class Program
         {
-            Console.WriteLine("Welcome to Logical Programs!!!");
-            Console.WriteLine("Press 1. for Fibonacci Series.");
-            Console.WriteLine("Press 2. for Perfect number.");
-            Console.WriteLine("Press 3. for Prime number.");
-            Console.WriteLine("Press 4. for Reverse number.");
-            Console.WriteLine("Press 5. to Stimulate Stop watch.");
-            //user input
-            int selection = Convert.ToInt32(Console.ReadLine());
-            //validating input 
-            while (selection < 1 || selection > 6)
+            static void Main(string[] args)
             {
-                Console.WriteLine("Invalid input");
-                Console.WriteLine("Enter a proper selection");
-                selection = Convert.ToInt32(Console.ReadLine());
-            }
-            //switch case
-            switch (selection)
-            {
-                case 1:
-                    //Fibonacci Series
-                    FibonacciSeries.Fibonacci();
-                    break;
+                Console.WriteLine("Welcome to Logical Programs!!!");
+                Console.WriteLine("Press 1. for Fibonacci Series.");
+                Console.WriteLine("Press 2. for Perfect number.");
+                Console.WriteLine("Press 3. for Prime number.");
+                Console.WriteLine("Press 4. for Reverse number.");
+                Console.WriteLine("Press 5. to Stimulate Stop watch.");
+                //user input
+                int selection = Convert.ToInt32(Console.ReadLine());
+                //validating input 
+                while (selection < 1 || selection > 6)
+                {
+                    Console.WriteLine("Invalid input");
+                    Console.WriteLine("Enter a proper selection");
+                    selection = Convert.ToInt32(Console.ReadLine());
+                }
+                //switch case
+                switch (selection)
+                {
+                    case 1:
+                        //Fibonacci Series
+                        FibonacciSeries.Fibonacci();
+                        break;
+                    case 2:
+                        //perfect number (6, 28, 496, 8128 )
+                        PerfectNumber perfect = new PerfectNumber();
+                        perfect.CheckPerfectNumber(28);
+                        break;
+                }
             }
         }
     }
